da370477633275bd4dfc899de059e93fdc4e6698
Stabilo <zteo015@e.ntu.edu.sg>
2024-01-05 13:07:03 +0800

    done expressions and if else

---
+++ expression_vs_statements.js
@@ -0,0 +1,17 @@
+// 05 Jan 2024, ZY
+// Expression vs Statements
+
+// expression is like a phrase
+// statement is like a sentence
+let number = 42;
+
+number = 1 + (number * 2);
+// for the above case, there are 6 expressions and 1 statement
+// 2 is an expression
+// number is an expression
+// (number * 2) is an expression
+// 1 is an expression
+// 1 + (number * 2) is an expression
+// number = 1 + (number * 2) is an expression
+
+// all 6 of the above expressions are part of a single statement
---
+++ if_else.js
@@ -0,0 +1,15 @@
+// 05 Jan 2024, ZY
+// If & Else
+
+let age = 42;
+
+if (age >= 18) {
+    goVote();
+}
+
+let isEnrolled = true;
+if (isEnrolled) {
+    takeClass();
+} else {
+    enrollFirst();
+}
